301c25a134a0483b3d4320c7110fd38b08eaa243
athenexfolk <folkadpocket99@gmail.com>
2023-12-29 23:37:48 +0700

    add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,35 +9,43 @@ import { Runner } from './socket/runner';
 import { OAuthStorage, provideOAuthClient } from 'angular-oauth2-oidc';
 import { storageFactory } from './core/auth/client-auth.service';
 import { loadingBarTrackInterceptor } from './core/interceptors/loading-bar-track.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
+    provideRouter(routes, withInMemoryScrolling({
         scrollPositionRestoration: 'enabled',
-      })
-    ),
-    provideHttpClient(
-      withInterceptors([
+    })),
+    provideHttpClient(withInterceptors([
         loadingBarTrackInterceptor,
         tokenInterceptor,
         publicEndpointInterceptor,
-      ])
-    ),
+    ])),
     {
-      provide: Runner,
-      useValue: Runner.getInstance(),
+        provide: Runner,
+        useValue: Runner.getInstance(),
     },
     provideOAuthClient({
-      resourceServer: {
-        allowedUrls: [],
-        sendAccessToken: true,
-      },
+        resourceServer: {
+            allowedUrls: [],
+            sendAccessToken: true,
+        },
     }),
     {
-      provide: OAuthStorage,
-      useFactory: storageFactory,
+        provide: OAuthStorage,
+        useFactory: storageFactory,
     },
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
