4c52fdfbec2c5aa5b05c11426a54e37fd44a15ee
athenexfolk <folkadpocket99@gmail.com>
2024-01-01 07:07:19 +0700

    add custom tag and tag filter search

--- src/app/shared/components/tags/tag-factory/tag-factory.component.ts
+++ src/app/shared/components/tags/tag-factory/tag-factory.component.ts
@@ -6,6 +6,7 @@ import { RemovableTagComponent } from '../removable-tag/removable-tag.component'
 import { Tag } from '../../../../core/models/tag';
 import { ClickInsideDirective } from '../../../directives/click-inside.directive';
 import { ClickOutsideDirective } from '../../../directives/click-outside.directive';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tag-factory',
@@ -15,6 +16,8 @@ import { ClickOutsideDirective } from '../../../directives/click-outside.directi
     RemovableTagComponent,
     ClickInsideDirective,
     ClickOutsideDirective,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './tag-factory.component.html',
   styleUrl: './tag-factory.component.scss',
@@ -24,16 +27,20 @@ export class TagFactoryComponent implements OnInit {
   @Output() tagChange = new EventEmitter<string[]>();
 
   availableTags = Tags;
+  filterAvailableTags = Tags;
+
   isTagSearchPanelOpen = false;
 
   usingTags: Tag[] = [];
   tagLimit = 5;
 
+  searchText = '';
+  maxTextLength = 24;
+
   constructor(private tagService: TagService) {}
 
   ngOnInit(): void {
     this.usingTags = [...this.existingTags];
-    // this.tagService
   }
 
   openTagSearchPanel() {
@@ -56,5 +63,31 @@ export class TagFactoryComponent implements OnInit {
       (tag) => tag.name !== removingTag.name
     );
     this.tagChange.emit(this.usingTags.map((tag) => tag.name));
+  }
+
+  searchTag(e: Event) {
+    this.searchText = this.searchText.trim().replace(/\s+/g, '');
+    (e.target as HTMLInputElement).value = this.searchText;
+    if (this.searchText.length > this.maxTextLength) {
+      this.searchText = this.searchText.slice(0, this.maxTextLength);
+      (e.target as HTMLInputElement).value = this.searchText;
+      return;
+    }
+
+    this.filterTag();
+  }
+
+  filterTag() {
+    this.filterAvailableTags = this.availableTags.filter((tag) =>
+      tag.name.includes(this.searchText)
+    );
+    if (
+      !this.searchText.length ||
+      !this.filterAvailableTags.find((tag) => tag.name === this.searchText)
+    ) {
+      this.filterAvailableTags.push(
+        this.tagService.mapUnknownTag(this.searchText)
+      );
+    }
   }
 }
