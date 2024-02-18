476bbef8e6cee6e42cbf4b1a679e2670476cb9b7
athenexfolk <folkadpocket99@gmail.com>
2024-02-18 15:14:50 +0700

    add user search

--- src/app/features/search/pages/one-search/one-search.component.ts
+++ src/app/features/search/pages/one-search/one-search.component.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PostPreviewAndAuthor } from '../../../../core/models/post';
 import { PostService } from '../../../../core/services/post.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Subject, debounceTime } from 'rxjs';
+import { Subject, debounceTime, tap } from 'rxjs';
 import { TagService } from '../../../../core/services/tag.service';
 import { Tag } from '../../../../core/models/tag';
 import { TagComponent } from '../../../../shared/components/tags/tag/tag.component';
 import { TopLovePostComponent } from '../../../read/feat/top-love-post/top-love-post.component';
 import { TopTagComponent } from '../../../read/feat/top-tag/top-tag.component';
+import { UserInformationService } from '../../../../core/services/user-information.service';
+import { Profile } from '../../../../core/models/user';
+import { UserHeaderComponent } from '../../../../shared/components/users/user-header/user-header.component';
 
 @Component({
   selector: 'app-one-search',
@@ -21,7 +24,8 @@ import { TopTagComponent } from '../../../read/feat/top-tag/top-tag.component';
     PostPreviewComponent,
     TagComponent,
     TopTagComponent,
-    TopLovePostComponent
+    TopLovePostComponent,
+    UserHeaderComponent
   ],
   templateUrl: './one-search.component.html',
   styleUrl: './one-search.component.scss',
@@ -33,12 +37,14 @@ export class OneSearchComponent {
 
   ppas: PostPreviewAndAuthor[] = [];
   tags: Tag[] = [];
+  users: Profile[] = [];
 
   private searchTextSubject = new Subject<void>();
 
   constructor(
     private postService: PostService,
-    private tagService: TagService
+    private tagService: TagService,
+    private userInfoService: UserInformationService
   ) {
     this.searchTextSubject.pipe(debounceTime(500)).subscribe(() => {
       this.search();
@@ -52,6 +58,11 @@ export class OneSearchComponent {
   search() {
     if (!this.searchText.length) return;
     this.isSearched = true;
+
+    this.userInfoService
+      .getProfilesByName(this.searchText)
+      .pipe(tap(console.log))
+      .subscribe((profiles) => (this.users = profiles));
 
     this.postService
       .getAllPosts({
